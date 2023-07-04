efd53ecf9277bf8d2a523ffd7624fab4ad6492cf
ilker senturk <68521925+ilkersenturk@users.noreply.github.com>
2023-07-04 18:52:34 -0400

    Update App.java
    my book name changed to mybook_changed

--- src/main/java/AutomationLibraray/adi/App.java
+++ src/main/java/AutomationLibraray/adi/App.java
@@ -9,41 +9,41 @@ public class App
          
     	
     	// datatype , objectname = new datatype;
-    	Book  mybook = new Book("Oliver","Oracle",123.45,500);
+    	Book  mybook_changed = new Book("Oliver","Oracle",123.45,500);
     	
     	System.out.println(mybook);
     	
     	
-    	String mymethodResult = mybook.toString();
+    	String mymethodResult = mybook_changed.toString();
     	
     	System.out.println(mymethodResult);
     	
     	//  print book title
-    	System.out.println(mybook.title);
+    	System.out.println(mybook_changed.title);
        
     	
     	// print remaining pages
 
-    	System.out.println(mybook.getRemainingPageCount());
+    	System.out.println(mybook_changed.getRemainingPageCount());
     	
     	// okunan sayfa
     	
     	mybook.setTotalfinishedPages(100);
     	
-    	System.out.println(mybook.getRemainingPageCount());
+    	System.out.println(mybook_changed.getRemainingPageCount());
     	
     	
     	// Before set price
-    	System.out.println(mybook.getPrice());
+    	System.out.println(mybook_changed.getPrice());
     	
     	// after setting price
     	mybook.setPrice(78.65);
-    	System.out.println(mybook.getPrice());    	
+    	System.out.println(mybook_changed.getPrice());    	
     	
     	mybook.discount();
     	
     	
-    	System.out.println(mybook.getPrice());
+    	System.out.println(mybook_changed.getPrice());
     	
     	
     }
