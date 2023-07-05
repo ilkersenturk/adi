0872a5d0495b7d4be0e3d18602dde9b0ba0e53a6
davutdogan <dvtdgn45@gmail.com>
2023-07-05 18:56:24 -0500

    Changes made for student class

--- src/main/java/AutomationLibraray/adi/ApplicationOfStudent.java
+++ src/main/java/AutomationLibraray/adi/ApplicationOfStudent.java
@@ -1,2 +1,31 @@
-package AutomationLibraray.adi;public class ApplicationOfStudent {
+package AutomationLibraray.adi;
+
+public class ApplicationOfStudent {
+    public static void main(String[] args) {
+        Student student=new Student("Real ","Madrid ","111 Caddesi Omaha,Nebraska ", 18, 1000.0,15 );
+        var s1=student.getName()+ student.getLastName()+student.getAdress()+student.getAge()+student.getBook()+student.getPoint();
+        System.out.println(s1);
+        System.out.println(student.getName());
+        System.out.println(student.getLastName());
+        System.out.println(student.getAdress());
+        System.out.println(student.getAge());
+        System.out.println(student.getBook());
+        System.out.println(student.getPoint());
+
+        System.out.println(student.getName());
+
+
+        student.setName("Vahhabi");
+        student.setLastName("Tarrikkkk");
+        student.setAdress("Abidin Dino Sokak no:61");
+        student.setAge(82);
+        student.setBook(82698.0);
+        student.setPoint(100000);
+        System.out.println(student.getName());
+        System.out.println(student.getLastName());
+        var s2=student.getName()+ student.getLastName()+student.getAdress();
+        System.out.println(s2);
+
+
+    }
 }
--- src/main/java/AutomationLibraray/adi/Student.java
+++ src/main/java/AutomationLibraray/adi/Student.java
@@ -1,26 +1,104 @@
 package AutomationLibraray.adi;
 
+import java.util.List;
+
 public class Student {
 
-	
-	/*
-	 * Every Student has
-	 * 
-	 * name : String
-	 * last name : String
-	 * address : String
-	 * age : integer
-	 * money : Credit Class object
-	 * books : List of Book Object
-	 * point : integer
-	 * 
-	 */
-	
-	
-	
-	
-	
-	
-	
-	
+
+    /*
+     * Every Student has
+     *
+     * name : String
+     * last name : String
+     * address : String
+     * age : integer
+     * money : Credit Class object
+     * books : List of Book Object
+     * point : integer
+     *
+     */
+    private String name;
+    private String lastName;
+    private String adress;
+    private int age;
+    private double book;
+    private int point;
+
+    private String quote;
+
+    //This method is constructor
+    Student(String studentName, String studentLastName, String studentAddress, int studentAge, double studentMoney, int studentPoint) {
+        name = studentName;
+        lastName = studentLastName;
+        adress = studentAddress;
+        age = studentAge;
+        book = studentMoney;
+        point = studentPoint;
+    }
+
+
+
+
+    //Getter method to get the name
+    public String getName() {
+        return this.name;
+    }
+
+    // Setter method to set the name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    //Getter method to get the lastName
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    //Setter method to set the lastName
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    //Getter method to get the address
+    public String getAdress() {
+        return this.adress;
+    }
+
+    //Setter method to set the address
+    public void setAdress(String address) {
+        this.adress = address;
+    }
+
+    //Getter method to get the age
+    public int getAge() {
+        return this.age;
+    }
+
+    //Setter method to set the age
+    public void setAge(int age) {
+        this.age = age;
+    }
+//Getter method to get the money
+    public double getBook(){
+        return this.book;
+    }
+
+    //Setter method to set the money
+    public void  setBook(double book){
+        this.book=book;
+    }
+
+    //Getter method to get the point
+    public int getPoint(){
+        return this.point;
+    }
+
+    //Setter method to set the point
+    public  void setPoint(int point){
+        this.point=point;
+    }
+
+    public  String getQuote(List<List<String>>quotes){
+        return this.quote;
+    }
 }
